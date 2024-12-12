e3a27868ff9ecc127f1fe177190a4d0d03104c04
Bobbydusanapudi <vvsatyadusanapudi@gmail.com>
2024-12-12 08:18:52 +0530

    second intial commit

--- run1.py
+++ run1.py
@@ -3,3 +3,5 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 sum_result = num1 + num2
 print(f"The sum of {num1} and {num2} is {sum_result}.")
+print("add the new line")
+
